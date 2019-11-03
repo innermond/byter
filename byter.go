f73fc355f7504479a44b7346bd097076340577e9
gabriel <innermond@yahoo.com>
2019-11-03 09:43:46 +0200

    bytes can be written even when error

--- byter.go
+++ byter.go
@@ -20,11 +20,14 @@ func (b *Buffer) WriteString(s string) (n int, err error) {
 	defer b.Mutex.Unlock()
 
 	n, err = io.WriteString(b.Writer, s)
+	// even in case of an error some bytes,
+	// maybe none,
+	// has the chance to be written
+	b.written += n
+
 	if err != nil {
 		return
 	}
-
-	b.written += n
 
 	return
 }
